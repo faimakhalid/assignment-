d618fefd2298864c4ba4635149694be23b27d965
faimakhalid <fatimakhalidjutt@gmail.com>
2024-10-01 05:48:31 -0700

    Add files via upload

---
+++ javascript question/script.js
@@ -0,0 +1,42 @@
+// 1. The alert() function displays a pop-up dialog box with a message.
+alert("This is an alert box!");
+
+// 2. Code to display an alert with a message.
+alert("Here is your message!");
+
+// 3. If you capitalize "Alert" like this:
+// Alert("This will cause an error.");
+// It will throw a "ReferenceError" because JavaScript is case-sensitive, and "alert" must be lowercase.
+
+// 4. The correct syntax to display "Hello World!" in an alert box.
+alert("Hello World!");
+
+// 5. Properly formatted alert statement with explanation.
+alert("Formatted Alert Example"); // 'alert' is a built-in function that displays a message in a dialog box.
+// "Formatted Alert Example" is the string message that will be displayed.
+
+// chapter 2
+
+// 1. Declare variable msg, assign "hello, world!", then display it in an alert.
+let msg = "hello, world!";
+alert(msg);
+
+// 2. Declare variable age, leave undefined, then assign 25 and display in an alert.
+let aged;
+age = 25;
+alert(age);
+
+// 3. Declare greeting, assign "Good morning!", display it, then change to "Good evening!" and display again.
+let greeting = "Good morning!";
+alert(greeting);
+greeting = "Good evening!";
+alert(greeting);
+
+// 4. Declare favoriteColor, assign "blue", and display in an alert.
+let favoriteColor = "blue";
+alert("My favorite color is " + favoriteColor);
+
+// 5. Declare age, leave undefined, assign 25 later, and display in an alert.
+let age;
+age = 25;
+alert(age);
